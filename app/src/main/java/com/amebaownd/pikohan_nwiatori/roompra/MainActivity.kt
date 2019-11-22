f1c94c8c0a65a9a8a53048c78bf56dcf0404cdb9
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-11-22 13:38:28 +0900

    add init

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/roompra/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/roompra/MainActivity.kt
@@ -2,11 +2,27 @@ package com.amebaownd.pikohan_nwiatori.roompra
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.message_editText)
+        val listView = findViewById<ListView>(R.id.message_list)
+
+        val addButton = findViewById<Button>(R.id.add_button)
+        addButton.setOnClickListener {
+
+            val stringArray = arrayOf("aaa")
+            val arrayAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,stringArray)
+            listView.adapter = arrayAdapter
+        }
+
     }
 }
